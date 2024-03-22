b0cbdb7d5a2142afbcffabb0ae253292f9bb8e51
Kat <63946129+khhuang7@users.noreply.github.com>
2024-03-22 16:10:18 -0500

    Update screen with default to-do list

--- src/index.js
+++ src/index.js
@@ -37,41 +37,72 @@ function createTodo (
   };
 }
 
-// TESTING
-// function log(text) {
-//   console.log(JSON.stringify(text));
-// }
-// log(defaultProject);
-// let testTodo = createTodo("Test todos", "Try out different functions");
-// defaultProject.addTodo(testTodo);
-// log(testTodo.getDueDate());
-// log("change due date");
-// testTodo.setDueDate("2024/04/30");
-// log(defaultProject.todos);
-// log(testTodo.getDueDate());
-// log("change title and completed");
-// testTodo.title = "Test todos including title change";
-// testTodo.toggleCompleted();
-// log(defaultProject.todos);
+function ScreenController () {
+  // load existing to-dos
+  const updateTodos = function (project) {
+    const contentDiv = document.querySelector(".content");
+    contentDiv.innerHTML = "";
+    const todos = project.todos;
+    
+    for (let todo of todos) {
+      const card = document.createElement("div");
+      card.classList.add("todo-card");
 
-// log("add new todo with date");
-// let testDate = createTodo("Test dates",
-//   "Check formatting for dates",
-//   "2024/03/31");
-// defaultProject.addTodo(testDate);
-// log(testDate);
-// log(testDate.getDueDate());
-// log(defaultProject.todos);
+      const cardTitle = document.createElement("h3");
+      cardTitle.innerText = todo.title;
+      card.appendChild(cardTitle);
+
+      const cardDescription = document.createElement("p");
+      cardDescription.innerText = todo.description;
+      card.appendChild(cardDescription);
+
+      contentDiv.appendChild(card);
+    }
+  }
+
+  return {
+    updateTodos,
+  }
+}
+
+// TESTING
+function log(text) {
+  console.log(JSON.stringify(text));
+}
+log(defaultProject);
+let testTodo = createTodo("Test todos", "Try out different functions");
+defaultProject.addTodo(testTodo);
+log(testTodo.getDueDate());
+log("change due date");
+testTodo.setDueDate("2024/04/30");
+log(defaultProject.todos);
+log(testTodo.getDueDate());
+log("change title and completed");
+testTodo.title = "Test todos including title change";
+testTodo.toggleCompleted();
+log(defaultProject.todos);
+
+log("add new todo with date");
+let testDate = createTodo("Test dates",
+  "Check formatting for dates",
+  "2024/03/31");
+defaultProject.addTodo(testDate);
+log(testDate);
+log(testDate.getDueDate());
+log(defaultProject.todos);
 
 // defaultProject.deleteTodo(1);
 // console.log(JSON.stringify(defaultProject.todos));
 
+ScreenController().updateTodos(defaultProject);
+
 /*
 TO DO - APPLICATION LOGIC:
-- Create view of all todos (by project or by calendar?)
+- Interface to add new todo
 - Expand a single todo to see/edit
 - Delete todo
 - Make look sexy
+- Create different project views
 - Add menu bar with all projects
 - Add calendar views
 
